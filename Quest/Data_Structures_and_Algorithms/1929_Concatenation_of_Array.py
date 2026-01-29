e6b337b2e80b80776f924478b151b86ce92d7098
Angel Alcántara <115179619+AngelUch27@users.noreply.github.com>
2026-01-29 14:04:34 -0600

    Add getConcatenation method to concatenate array
    Implement concatenation of an array in the Solution class.

---
+++ Quest/Data_Structures_and_Algorithms/1929_Concatenation_of_Array.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = nums+nums
+        return ans
+        
