34f499c3b672660a9c66a98f25850f3f57386c00
Angel Alcántara <115179619+AngelUch27@users.noreply.github.com>
2026-02-26 12:30:41 -0600

    Add SQL query for average processing time per machine

---
+++ SQL_50/# 1661_Average_Time_of_process_per_Machine.sql
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/average-time-of-process-per-machine/?envType=study-plan-v2&envId=top-sql-50
+# 1661. Average Time of Process per Machine
+SELECT
+    s.machine_id,
+    ROUND(AVG(e.timestamp - s.timestamp), 3) AS processing_time
+FROM Activity s
+JOIN Activity e
+  ON s.machine_id = e.machine_id
+ AND s.process_id = e.process_id
+WHERE s.activity_type = 'start'
+  AND e.activity_type = 'end'
+GROUP BY s.machine_id;
